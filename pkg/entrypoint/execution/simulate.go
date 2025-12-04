d043f04f069946d3655a0f437442b5cd50b282ae
robinsdan <robinsdan1111@hotmail.com>
2025-12-04 14:42:33 +0800

    more log

--- pkg/entrypoint/execution/simulate.go
+++ pkg/entrypoint/execution/simulate.go
@@ -1,11 +1,15 @@
 package execution
 
 import (
+	"context"
 	"fmt"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/laizy/log"
 	"github.com/robinsdan/sweet"
 	"github.com/stackup-wallet/stackup-bundler/pkg/entrypoint"
 	"github.com/stackup-wallet/stackup-bundler/pkg/entrypoint/reverts"
@@ -23,8 +27,21 @@ type SimulateInput struct {
 	Data   []byte
 }
 
+type Client struct {
+	*ethclient.Client
+}
+
+func (self *Client) CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
+	log.Infof("calling contract, call msg: %v", sweet.JsonStr(msg))
+	val, err := self.Client.CallContract(ctx, msg, blockNumber)
+	log.Infof("call result, val:%x, err: %v", val, err)
+
+	return val, err
+}
+
 func SimulateHandleOp(in *SimulateInput) (*reverts.ExecutionResultRevert, error) {
-	ep, err := entrypoint.NewEntrypoint(in.EntryPoint, ethclient.NewClient(in.Rpc))
+	client := ethclient.NewClient(in.Rpc)
+	ep, err := entrypoint.NewEntrypoint(in.EntryPoint, &Client{client})
 	if err != nil {
 		return nil, err
 	}
